34166d20df111a2a65919b5d5f72df354e61ae59
LiYang <yang.li@meiauto.cn>
2018-03-05 11:05:47 +0800

    add RxBinding

--- sample/src/main/java/cn/meiauto/matcommon/sample/MainActivity.java
+++ sample/src/main/java/cn/meiauto/matcommon/sample/MainActivity.java
@@ -3,6 +3,8 @@ package cn.meiauto.matcommon.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.reactivex.Observable;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
